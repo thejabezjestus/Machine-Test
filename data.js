6b0499f3e55dc92639f8fbfac85a5a8ee4025227
thejabezjestus <planojest@gmail.com>
2025-01-23 15:48:27 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -13,3 +13,13 @@ function validate(){
     alert("Login failed")
     } 
 }
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+      const navbarToggler = document.querySelector('.navbar-toggler');
+      const navbarCollapse = document.querySelector('#navbarNav');
+
+      navbarToggler.addEventListener('click', function () {
+          navbarCollapse.classList.toggle('show');
+      });
+  });
